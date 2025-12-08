ccbe66973adaeabc485f9b91dcd4a402692ee09a
mzhangxy <96800168+mzhangxy@users.noreply.github.com>
2025-12-08 12:54:45 +0800

    Set default port values in start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 set -e
 
 # ================== 端口设置 ==================
-export TUIC_PORT=${TUIC_PORT:-""}
-export HY2_PORT=${HY2_PORT:-""}
-export REALITY_PORT=${REALITY_PORT:-""}
+export TUIC_PORT=${TUIC_PORT:-"56123"}
+export HY2_PORT=${HY2_PORT:-"56124"}
+export REALITY_PORT=${REALITY_PORT:-"56125"}
 
 # ================== 强制切换到脚本所在目录 ==================
 cd "$(dirname "$0")"
